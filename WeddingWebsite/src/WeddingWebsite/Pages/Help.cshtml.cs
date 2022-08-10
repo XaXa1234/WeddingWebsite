f126ec1f5b792071bfae4a6aaae0c2f85510413b
XaXa urtiz <urtizverea@hotmail.fr>
2022-08-10 08:45:49 +0100

    first commit

---
+++ WeddingWebsite/src/WeddingWebsite/Pages/Help.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingWebsite.Pages
+{
+    public class HelpModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
