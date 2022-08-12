b62384474e0300bf53af53083cff003055f10af3
XaXa urtiz <urtizverea@hotmail.fr>
2022-08-12 21:14:41 +0100

    start

---
+++ src/WeddingWebsite/Pages/WeddingVenue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingWebsite.Pages
+{
+    public class WeddingVenueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
